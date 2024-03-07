959a10815d528ad1aa1dc5a5194441d1272dd6df
Davidsb04 <caisilvasb756@hotmail.com>
2024-03-07 19:11:14 -0300

    Adicionar arquivos de projeto.

---
+++ InverterPalavras/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace InverterPalavras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra, palavraAoContrario = "";
+
+            Console.Write("Digite uma palavra: ");
+            palavra = Console.ReadLine().ToLower();
+
+            char[] vetor = palavra.ToCharArray();
+
+
+            for (int i = vetor.Length - 1; i >= 0; i--)
+            {
+                palavraAoContrario += vetor[i];
+            }
+
+            Console.WriteLine($"\nA palavra {palavra} ao contrário é: {palavraAoContrario}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProcessoTargetsistemas/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SequenciaFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0, a = 0, b = 1, limite = 17;
+            bool contemNumero = false;
+
+            Console.Write("\nInforme o número que deseja pesquisar (até 2584): ");
+            int numPesquisa = int.Parse(Console.ReadLine());
+
+            while (cont < limite)
+            {
+                int n = a;
+                a = b;
+                b = n + b;
+                if (numPesquisa == b)
+                {
+                    contemNumero = true;
+                }
+                cont++;
+                Console.WriteLine(b);
+            }
+
+            if (contemNumero)
+            {
+                Console.WriteLine("\nA sequência de Fibonacci contém o número que você informou.");
+            }
+            else
+            {
+                Console.WriteLine("\nA sequência de Fibonacci não contém o número que você informou.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
